09b979d7b16af0a8efd23389e48b9fa03cddfc8f
raquelmu <raquel.muniesa@gmail.com>
2020-03-14 13:07:10 +0100

    testing

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose
 		useUnifiedTopology: true,
 	})
 	.then(() => {
-		console.log(`conected to ${dbPath}`);
+		console.log(`Conected to Wally...`);
 	})
 	.catch(error => {
 		console.error(error);
