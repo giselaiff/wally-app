920bf91116c5b4f25d31d1d688d7f56255391916
raquelmu <raquel.muniesa@gmail.com>
2020-03-17 10:31:09 +0100

    See one event

--- routes/events.js
+++ routes/events.js
@@ -33,22 +33,53 @@ router.post('/add', (req, res, next) => {
 router.get('/', (req, res, next) => {
 	const { currentUser } = req.session;
 	Event.find()
-		.then(event => {
+		.then(events => {
 			res.render('events/list', {
-				event,
+				events,
 				currentUser,
 				info: req.flash('info'),
 			});
 		})
 		.catch(next);
 });
+
+// POST see one event:  /event/:id
+
+router.get('/:id', (req, res, next) => {
+    const paramEventId = req.params.id;
+    Event.findOne({_id: paramEventId})
+    .then (singleEvent => {
+        res.render('events/singleEvent', {eventData: singleEvent})
+    })
+    .catch(next);
+
+});
+/*
+router.post('/:id/update', (req, res, next) => {
+	const { id } = req.params;
+	const { name, description, hour } = req.body;
+	Event.findByIdAndUpdate(id, {
+		name,
+		description,
+		hour,
+	})
+		.then(eventUpdated => {
+			res.redirect('/singleEvent');
+		})
+		.catch(next);
+});
+
+*/
+
+
+
 module.exports = router;
 
 
 
 
-//| POST | /event/:id  | See one event  |
-//| GET | /event/add  | Create an event  |
+
+
 //| GET | /event/:id/update  | Update event info  |
 //| POST | /event/:id  | Send updated event info  |
 //| POST | /event/:id/delete  | Delete an event  |
