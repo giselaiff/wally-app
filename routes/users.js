76f9390b2cc0f43bcc630b42da2e8efd829f17bd
Gisela García José <gisgarjos@hotmail.com>
2020-03-21 13:42:37 +0100

    mergeando2

--- routes/users.js
+++ routes/users.js
@@ -10,36 +10,5 @@ const User = require('../models/User');
     const users = req.app.locals.users;
     const username = req.params.username;
   
-    users.findOne({ username }, (err, results) => {
-      if (err || !results) {
-        res.render('public-profile', { messages: { error: ['User not found'] } });
-      }
-  
-      res.render('public-profile', { ...results, username });
-    });
-  })
-  // --------------------------------------------------
-  
-  
-  // Handle updating user profile data
-  // --------------------------------------------------
-  router.post('/', (req, res, next) => {
-    if (!req.isAuthenticated()) {
-      res.redirect('/auth/login');
-    }
-  
-    const users = req.app.locals.users;
-    const { name, github, twitter, facebook } = req.body;
-    const _id = ObjectID(req.session.passport.user);
-  
-    users.updateOne({ _id }, { $set: { name, github, twitter, facebook } }, (err) => {
-      if (err) {
-        throw err;
-      }
-      
-      res.redirect('/users');
-    });
-  });
-  // --------------------------------------------------
   
   module.exports = router;
